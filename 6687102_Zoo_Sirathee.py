e27820ed03eb8f5d7b7e789241c7443cd4c33656
siratheenext <siratheenext@gmail.com>
2024-11-11 12:21:01 +0700

    6687102_Zoo_Sirathee.py

--- 6687102_Zoo_Sirathee.py
+++ 6687102_Zoo_Sirathee.py
@@ -15,15 +15,28 @@ class TestZoo(unittest.TestCase):
 
     def test_boys_girls_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+        self.assertEqual(self.zoo.get_ticket_price(1), 50)
+        self.assertEqual(self.zoo.get_ticket_price(11), 50)
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
 
     def test_young_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(15), 100)
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+        self.assertEqual(self.zoo.get_ticket_price(14), 100)
+        self.assertEqual(self.zoo.get_ticket_price(19), 100)
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
 
     def test_adult_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(25), 150)
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+        self.assertEqual(self.zoo.get_ticket_price(22), 150)
+        self.assertEqual(self.zoo.get_ticket_price(59), 150)
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
 
     def test_old_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(195), 100)
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
     
 if __name__ == '__main__': 
     unittest.main()
